0fc508beff76aee7a51768bc2a374cd9954a221c
Skczu <72468833+Skczu@users.noreply.github.com>
2022-10-14 16:35:39 +0200

    refactor: remove redundant Fragment

--- components/composited/AccountPage/AccountPage.tsx
+++ components/composited/AccountPage/AccountPage.tsx
@@ -1,16 +1,13 @@
-import { Fragment } from 'react';
 import { AccountForm as AccountSection } from '../../generic/AccountForm/AccountForm';
 import { Heading } from '../../generic/Heading/Heading';
 import { PasswordForm as PasswordSection } from '../../generic/PasswordForm/PasswordForm';
 
 export const AccountPage = () => {
   return (
-    <Fragment>
-      <section className="flex flex-col gap-4">
-        <Heading as="h1" variant="base" content="Konto użytkownika" bottomBorder={true} />
-        <AccountSection />
-        <PasswordSection />
-      </section>
-    </Fragment>
+    <section className="flex flex-col gap-4">
+      <Heading as="h1" variant="base" content="Konto użytkownika" bottomBorder={true} />
+      <AccountSection />
+      <PasswordSection />
+    </section>
   );
 };
