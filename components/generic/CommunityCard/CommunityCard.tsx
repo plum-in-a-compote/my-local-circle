a54ceeceda6e0cc1fc556b64a058158cefaddd81
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2022-10-18 10:45:39 +0200

    feat: initial changes on communities page

--- components/generic/CommunityCard/CommunityCard.tsx
+++ components/generic/CommunityCard/CommunityCard.tsx
@@ -1,4 +1,7 @@
+import { AccountIcon } from '../Icons/AccountIcon';
 import { CommunityIcon } from '../Icons/CommunityIcon';
+import { LocationFilledIcon } from '../Icons/LocationFilledIcon';
+import { ProjectIcon } from '../Icons/ProjectIcon';
 
 type CommunityCardProps = {
   name: string;
@@ -22,7 +25,20 @@ export const CommunityCard = ({
         <h2 className="text-sm font-semibold text-blue-800">{name}</h2>
       </div>
       <p className="text-xs leading-5 font-normal text-gray-700">{description}</p>
-      <div></div>
+      <div className="flex gap-5">
+        <div className="flex items-center gap-1">
+          <LocationFilledIcon className="w-5 h-5 fill-amber-600" />
+          <span className="text-xs font-normal text-gray-700">{city}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <ProjectIcon className="w-5 h-5" />
+          <span className="text-xs font-normal text-gray-700">{projectNo}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <AccountIcon className="w-5 h-5" />
+          <span className="text-xs font-normal text-gray-700">{usersNo}</span>
+        </div>
+      </div>
     </article>
   );
 };
