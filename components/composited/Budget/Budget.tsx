d18d0edd220a05de9b0cde1c134a1dbccf057ae7
Skczu <72468833+Skczu@users.noreply.github.com>
2022-10-20 19:21:23 +0200

    feat: continue working on components

--- components/composited/Budget/Budget.tsx
+++ components/composited/Budget/Budget.tsx
@@ -1,27 +1,44 @@
 import { Heading } from '../../generic/Heading/Heading';
 import { Text } from '../../generic/Text/Text';
+import { Textarea } from '../../generic/Textarea/Textarea';
+import { BudgetInfoList } from './BudgetInfoList';
 
-type BudgetProps = {
+type BudgetFields = {
   title: string;
   description: string;
   estimatedCost: string;
-  estimatedRealisationDate: string;
+  estimatedRealizationDate: string;
   place: string;
   // TO BE DISCUSSED:
   // costPerPerson
 };
 
-export const Budget = ({
-  title,
-  description,
-  estimatedCost,
-  estimatedRealisationDate,
-  place,
-}: BudgetProps) => {
+type BudgetProps = {
+  fields: BudgetFields;
+};
+
+export const Budget = ({ fields }: BudgetProps) => {
   return (
     <article>
-      <Heading content={title} className="font-semibold" as="h2" variant="base" />
-      <Text as="p" content={description} />
+      <Heading className="mb-3" content={fields.title} as="h2" variant="baseSemibold" />
+      <Text className="mb-6 whitespace-pre-wrap w-5/6" as="p" content={fields.description} />
+      <BudgetInfoList
+        items={[
+          {
+            label: 'Przewidywany koszt',
+            value: fields.estimatedCost,
+          },
+          {
+            label: 'Data realizacji',
+            value: fields.estimatedRealizationDate,
+          },
+          {
+            label: 'Miejsce',
+            value: fields.place,
+          },
+        ]}
+      />
+      <Textarea name="test" label="Test" />
     </article>
   );
 };
--- components/composited/Budget/BudgetInfoList.tsx
+++ components/composited/Budget/BudgetInfoList.tsx
@@ -7,6 +7,15 @@ type BudgetInfoListProps = {
   items: BudgetInfoItem[];
 };
 
-export const BudgetInfoList = ({}: BudgetInfoListProps) => {
-  return <ul className="list-none">{/* map */}</ul>;
+export const BudgetInfoList = ({ items }: BudgetInfoListProps) => {
+  return (
+    <ul className="border-y border-gray-300 list-none">
+      {items.map((item) => (
+        <li key={item.label} className="flex justify-between px-3 py-2.5 odd:bg-gray-50">
+          <span className="text-xs leading-4 font-normal text-gray-700">{item.label}</span>
+          <span className="text-xs leading-4 font-semibold text-gray-800">{item.value}</span>
+        </li>
+      ))}
+    </ul>
+  );
 };
