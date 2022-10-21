e70e23e774c20ff39e808b47f4f0c2e6ef5c6a9b
Skczu <72468833+Skczu@users.noreply.github.com>
2022-10-21 19:25:07 +0200

    fix: adjust according to review

--- components/composited/Notification/NotificationItem.tsx
+++ components/composited/Notification/NotificationItem.tsx
@@ -3,7 +3,11 @@ import { Fragment } from 'react';
 import { Badge } from '../../generic/Badge/Badge';
 import { LinkButton } from '../../generic/LinkButton/LinkButton';
 import { Text } from '../../generic/Text/Text';
-import { getNotificationContents, NotificationType } from './getNotificationContents';
+import {
+  getNotificationTitle,
+  getNotificationDescription,
+  NotificationType,
+} from './getNotificationContents';
 
 type NotificationItemProps = {
   notificationType: NotificationType;
@@ -16,13 +20,14 @@ export const NotificationItem = ({
   communityName,
   isNew = false,
 }: NotificationItemProps) => {
-  const [name, descPart1, descPart2] = getNotificationContents(notificationType);
+  const title = getNotificationTitle(notificationType);
+  const [descPart1, descPart2] = getNotificationDescription(notificationType);
 
   return (
     <Fragment>
       <div className="flex items-center mb-2 lg:mb-3">
         <span className="pr-3 text-xs text-gray-800 leading-4 font-semibold sm:text-sm sm:leading-4 lg:leading-5 lg:text-lg">
-          {name}
+          {title}
         </span>
         {isNew && <Badge textContent="Nowe" color="amber" />}
       </div>
--- components/composited/Notification/getNotificationContents.ts
+++ components/composited/Notification/getNotificationContents.ts
@@ -4,15 +4,28 @@ export type NotificationType =
   | 'newProject'
   | 'projectVotingComplete';
 
-export const getNotificationContents = (notificationName: NotificationType) => {
+export const getNotificationTitle = (notificationName: NotificationType) => {
   switch (notificationName) {
     case 'applicationAccepted':
-      return ['Akceptacja podania', 'Twoje podanie do grupy ', ' zostało pomyślnie rozpatrzone.'];
+      return 'Akceptacja podania';
     case 'newApplication':
-      return ['Nowe podanie', 'Pojawiło się nowe podanie do twojej grupy ', '.'];
+      return 'Nowe podanie';
     case 'newProject':
-      return ['Nowy projekt', 'Utworzono nowy projekt zagospodarowania budżetu w grupie ', '.'];
+      return 'Nowy projekt';
     case 'projectVotingComplete':
-      return ['Zakończenie głosowania', 'Projekt w grupie ', ' oczekuje rozpatrzenia.'];
+      return 'Zakończenie głosowania';
+  }
+};
+
+export const getNotificationDescription = (notificationName: NotificationType) => {
+  switch (notificationName) {
+    case 'applicationAccepted':
+      return ['Twoje podanie do grupy ', ' zostało pomyślnie rozpatrzone.'];
+    case 'newApplication':
+      return ['Pojawiło się nowe podanie do twojej grupy ', '.'];
+    case 'newProject':
+      return ['Utworzono nowy projekt zagospodarowania budżetu w grupie ', '.'];
+    case 'projectVotingComplete':
+      return ['Projekt w grupie ', ' oczekuje rozpatrzenia.'];
   }
 };
