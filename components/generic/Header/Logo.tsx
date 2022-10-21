bdea80fe3280993bdd5f798f43743f7ac175f737
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2022-10-21 22:01:19 +0200

    feat: fix logo path

--- components/generic/Header/Logo.tsx
+++ components/generic/Header/Logo.tsx
@@ -3,9 +3,9 @@ export const Logo = () => {
     <div className="hidden sm:block w-9 h-9 sm:w-12 sm:h-12">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="logo-lg.png"
+        src="/logo-lg.png"
         alt="Logo projektu My local circle."
-        srcSet="logo-sm.png 36w, logo.png 48w, logo-lg.png"
+        srcSet="/logo-sm.png 36w, /logo.png 48w, /logo-lg.png"
       />
     </div>
   );
