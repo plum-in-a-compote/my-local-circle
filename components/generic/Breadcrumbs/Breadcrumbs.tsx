1f88121967c8a12b0360e21348ccca4f032771db
Skczu <72468833+Skczu@users.noreply.github.com>
2022-10-23 22:05:15 +0200

    fix: add flex wrap

--- components/generic/Breadcrumbs/Breadcrumbs.tsx
+++ components/generic/Breadcrumbs/Breadcrumbs.tsx
@@ -15,14 +15,14 @@ type BreadcrumbsProps = {
 
 export const Breadcrumbs = ({ community, budget, className }: BreadcrumbsProps) => {
   return (
-    <div className={cx('flex gap-1', className)}>
-      <span className="text-sm leading-5 font-normal text-blue-800 break-all focus:underline hover:underline lg:text-base">
+    <div className={cx('flex flex-wrap gap-1', className)}>
+      <span className="text-sm leading-5 font-normal text-blue-800 focus:underline hover:underline lg:text-base">
         <Link href={`/communities/${community.slug}`}>
           <a>{community.name}</a>
         </Link>
       </span>
       <span className="text-sm leading-5 font-normal text-gray-700 lg:text-base">/</span>
-      <span className="text-sm leading-5 font-semibold text-blue-800 break-all lg:text-base focus:underline hover:underline">
+      <span className="text-sm leading-5 font-semibold text-blue-800 lg:text-base focus:underline hover:underline">
         <Link href={`/communities/${community.slug}/budgets/${budget.slug}`}>
           <a>{budget.name}</a>
         </Link>
