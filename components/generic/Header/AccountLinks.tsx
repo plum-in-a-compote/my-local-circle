35ee289987b5e529f136d84d4f1472ccab7f04fb
Skczu <72468833+Skczu@users.noreply.github.com>
2022-10-23 22:49:53 +0200

    fix: change links order

--- components/generic/Header/AccountLinks.tsx
+++ components/generic/Header/AccountLinks.tsx
@@ -4,20 +4,20 @@ import { NotificationIcon } from '../Icons/NotificationIcon';
 export const AccountLinks = () => {
   return (
     <div className="flex gap-3">
-      <Link href="/account">
-        <a
-          className="transition group block p-0.5 rounded-full hover:bg-blue-100 focus:bg-blue-100"
-          aria-label="Przejdź do ustawień konta."
-        >
-          <AccountIcon className="transition group-hover:fill-blue-800 group-focus:fill-blue-800" />
-        </a>
-      </Link>
       <Link href="/notifications">
         <a
           className="transition group block p-0.5 rounded-full hover:bg-blue-100 focus:bg-blue-100"
           aria-label="Przejź do powiadomień."
         >
           <NotificationIcon className="transition group-hover:fill-blue-800 group-focus:fill-blue-800" />
+        </a>
+      </Link>
+      <Link href="/account">
+        <a
+          className="transition group block p-0.5 rounded-full hover:bg-blue-100 focus:bg-blue-100"
+          aria-label="Przejdź do ustawień konta."
+        >
+          <AccountIcon className="transition group-hover:fill-blue-800 group-focus:fill-blue-800" />
         </a>
       </Link>
     </div>
